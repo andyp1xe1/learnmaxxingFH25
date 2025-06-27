a49bf036dd247a6f7e885e6c56503821c03f82b5
Andre Chicu <andriesh.chicu@gmail.com>
2025-06-27 21:26:06 +0300

    fix: delete frontend junk

--- learnmaxxing/src/App.tsx
+++ learnmaxxing/src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
     </>
   )
 }
